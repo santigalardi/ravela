c35b43c8493c590b6b4051de99cef66ada25a7f2
Santiago Galardi <santygalardi@gmail.com>
2024-07-28 19:10:36 -0300

    updated animation styles

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 
 const MainBanner = () => (
-  <section className="min-h-screen flex justify-center items-center bg-cover bg-center mb-20">
-    <video autoPlay loop muted playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0">
+  <section className="relative min-h-screen flex justify-center items-center bg-cover bg-center mb-20 overflow-hidden">
+    <video autoPlay loop muted playsInline className="absolute inset-0 w-full h-full object-cover z-0">
       <source src="/videos/textura-background.mp4" type="video/mp4" />
     </video>
-    <div className="p-6 md:p-0 md:pb-16 z-10">
+    <div className="relative p-6 md:p-0 md:pb-16 z-10">
       <Image src="/logo-completo-slogan-naranja.png" width={800} height={800} alt="Ravela Logo" priority />
     </div>
   </section>
