59be35438b2231159cb9a9bc36a4ececdf6c751b
Santiago Galardi <santygalardi@gmail.com>
2024-08-30 20:51:52 -0300

    scroll to up button & navbar styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import MainBanner from '@/components/MainBanner';
 import ContestSection from '@/components/ContestSection';
 import LastSetSection from '@/components/LastSetSection';
 import Footer from '@/components/Footer';
+import NavBar from '@/components/NavBar';
+import ScrollToUpButton from '@/components/ScrollToUpButton';
 
 export default function Home() {
   return (
     <main className="min-w-[320px]">
+      <NavBar />
       <MainBanner />
       <LastSetSection />
       <FAQ />
--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -2,14 +2,13 @@ import Image from 'next/image';
 import NavBar from './NavBar';
 
 const MainBanner = () => (
-  <section className="relative min-h-screen flex justify-center items-center overflow-hidden">
-    <NavBar />
+  <section id="main-banner" className="min-h-screen flex justify-center items-center overflow-hidden m-0">
     <h1 className="sr-only">Ravela</h1>
     <h2 className="sr-only">Ravela - Próximos Eventos de Música Electrónica</h2>
     <video autoPlay loop muted playsInline className="absolute inset-0 w-full h-full object-cover">
       <source src="/videos/textura-background.mp4" type="video/mp4" />
     </video>
-    <div className="relative p-6 md:p-0 md:pb-16 z-10">
+    <div className="relative px-6 md:p-0 md:pb-16">
       <Image src="/wordmark-white.png" width={800} height={800} alt="Ravela Logo" priority />
     </div>
   </section>
