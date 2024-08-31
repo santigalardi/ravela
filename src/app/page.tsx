64a812fd42082b93f168cc731a389e16b9a06793
Santiago Galardi <santygalardi@gmail.com>
2024-08-31 17:45:54 -0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Countdown from '@/components/Countdown';
-import FAQ from '@/components/FAQ';
-import MainBanner from '@/components/MainBanner';
-import ContestSection from '@/components/ContestSection';
-import LastSetSection from '@/components/LastSetSection';
+import FAQ from '@/sections/FAQ';
+import MainBanner from '@/sections/MainBanner';
+import ContestSection from '@/sections/ContestSection';
+import LastSetSection from '@/sections/LastSetSection';
 import Footer from '@/components/Footer';
 import NavBar from '@/components/NavBar';
-import ScrollToUpButton from '@/components/ScrollToUpButton';
 
 export default function Home() {
   return (
